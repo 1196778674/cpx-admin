a83f5b799c8b2d98e50fe058033a78653e6ea163
liufei <liufei@chupinxiu.com>
2016-09-05 10:30:16 +0800

    router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,68 +24,34 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
                 'left@index': {
                     templateUrl: 'views/partials/left.html'
                 },
-                'right@index': {
+                'main@index': {
                     templateUrl: 'views/partials/right.html'
                 }
             }
         })
-        // .state('index.usermng', {
-        //     url: '/usermng',
-        //     views: {
-        //         'main@index': {
-        //             templateUrl: 'tpls3/usermng.html',
-        //             controller: function($scope, $state) {
-        //                 $scope.addUserType = function() {
-        //                     $state.go("index.usermng.addusertype");
-        //                 }
-        //             }
-        //         }
-        //     }
-        // })
-        // .state('index.usermng.highendusers', {
-        //     url: '/highendusers',
-        //     templateUrl: 'tpls3/highendusers.html'
-        // })
-        // .state('index.usermng.normalusers', {
-        //     url: '/normalusers',
-        //     templateUrl: 'tpls3/normalusers.html'
-        // })
-        // .state('index.usermng.lowusers', {
-        //     url: '/lowusers',
-        //     templateUrl: 'tpls3/lowusers.html'
-        // })
-        // .state('index.usermng.addusertype', {
-        //     url: '/addusertype',
-        //     templateUrl: 'tpls3/addusertypeform.html',
-        //     controller: function($scope, $state) {
-        //         $scope.backToPrevious = function() {
-        //             window.history.back();
-        //         }
-        //     }
-        // })
-        // .state('index.permission', {
-        //     url: '/permission',
-        //     views: {
-        //         'main@index': {
-        //             template: '这里是权限管理'
-        //         }
-        //     }
-        // })
-        // .state('index.report', {
-        //     url: '/report',
-        //     views: {
-        //         'main@index': {
-        //             template: '这里是报表管理'
-        //         }
-        //     }
-        // })
-        // .state('index.settings', {
-        //     url: '/settings',
-        //     views: {
-        //         'main@index': {
-        //             template: '这里是系统设置'
-        //         }
-        //     }
-        // })
+        .state('index.storemanagement', {
+            url: '/storemanagement',
+            views: {
+                'main@index': {
+                    templateUrl: 'views/storemanagement.html'
+                }
+            }
+        })
+        .state('index.contractedstores', {
+            url: '/contractedstores',
+            views: {
+                'main@index': {
+                    templateUrl: 'views/contractedstores.html'
+                }
+            }
+        })
+        .state('index.datastatistics', {
+            url: '/datastatistics',
+            views: {
+                'main@index': {
+                    templateUrl: 'views/datastatistics.html'
+                }
+            }
+        })
 });
 
