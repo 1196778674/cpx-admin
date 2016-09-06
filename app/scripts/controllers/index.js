5812be20dc83205928d561bd20b0ec4d245f3f04
liufei <liufei@chupinxiu.com>
2016-09-06 10:07:20 +0800

    factory

---
+++ app/scripts/controllers/index.js
@@ -0,0 +1,18 @@
+window.routerApp.factory('httpServer', ['$http',function ($http) {
+	var getDataList = function(url,scope){
+		$http.get(url).success(function(data){
+			scope.dataList = data;
+		})
+	}
+
+	return {
+		getDataList: getDataList
+	};
+}])
+window.routerApp.controller('indexCtrl', ['$scope','$http','httpServer', function ($scope,$http,httpServer) {
+	$scope.dateTime = '2016-9-5';
+	$scope.show = function(){
+		$scope.dateTime = new Date();
+	}
+	httpServer.getDataList('json/index.json',$scope);
+}])
