b9f2004f1b00fc88edfced9fe91fd0cb215b5b6a
liufei <liufei@chupinxiu.com>
2016-09-07 11:30:31 +0800

    datepicker

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -12,10 +12,23 @@ window.routerApp.factory('httpServer', ['$http',function ($http) {
 	};
 }])
 
+window.routerApp.directive('datepicker', [function () {
+	return {
+		restrict: 'A',
+		link: function (scope, iElement, iAttrs) {
+			scope.startTime = new Date();
+			$("." + iAttrs.class).datetimepicker({
+	        format: "yyyy-mm-dd",
+	        autoclose: true,
+	        todayBtn: true,
+	        startDate: scope.startTime,
+	        minuteStep: 10
+	    });
+		}
+	};
+}])
 
 window.routerApp.controller('indexCtrl', ['$scope','$http','httpServer', function ($scope,$http,httpServer) {
-	$scope.startTime = new Date();
-	$scope.endTime = new Date();
 	$scope.show = function(){
 		$scope.dateTime = new Date();
 	}
